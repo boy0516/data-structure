f9473e79101528ada8bb2bd5c573043bf92dd977
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-06-30 00:33:46 +0900

    0630

--- javascript_study/app.js
+++ javascript_study/app.js
@@ -1,13 +1,19 @@
-const age = prompt("How old are you?");
+// document.title="Hello! From JS!"
+// // html이 아니라 js가 title로 적용될 것
 
-if(isNaN(age) || age<0){
-    console.log("Please write a number");
-} else if(age <18){
-    console.log("You are too young");
-} else if(age>=18 && age<=50){
-    console.log("you can drink");
-} else if(age>50 &&age<=80){
-    console.log("You should exercise");
-} else if (age> 80){
-    console.log("You can do whatever you want");
-} 
+// const title = document.getElementById("something");
+
+// title.innerText = "Got ya!"
+
+// console.log(title.className);
+// const hellos = document.getElementsByClassName("hello");
+
+// console.log(title);
+
+// const title = document.getElementsByTagName("h1");
+
+const title = document.querySelector("div.hello:first-child h1");
+console.log(title); 
+title.innerText = "Hello";
+const title_sec = document.querySelector("div.hello:second-child h1");
+title_sec.innerText = "second greetings";
